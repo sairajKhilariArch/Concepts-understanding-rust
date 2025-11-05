d6f915f50be3e1c01c5933292e847eb4b32f2cf7
SAIRAJ KHILARI <sairajkhilari191918110+github1@gmail.com>
2025-11-05 20:58:26 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -28,18 +28,22 @@
 
 // use std::collections::{hash_map, HashMap};
 
+fn main() {
+    //     let mut hashmappp = HashMap :: new();
+    //     hashmappp.insert ("sairaj" ,655);
+    //     hashmappp.insert ("soham" ,677);
+    //     hashmappp.insert ( "ankit" , 688);
 
-fn main(){
-//     let mut hashmappp = HashMap :: new();
-//     hashmappp.insert ("sairaj" ,655);
-//     hashmappp.insert ("soham" ,677);
-//     hashmappp.insert ( "ankit" , 688);
+    //     println!("{:?}",hashmappp);
 
+    // let var1 = vec![1, 2, 3, 4, 5, 6, 0];
+    // let get: &Vec<i32> = &var1.iter().map(|f| f * f).collect();
+    // print!("{:?}", get);
+    let mut sss = 555;
+    {
+        let sss = 888;
+        println!("{sss}")
 
-//     println!("{:?}",hashmappp); 
-
-    let mut var1 = vec![1, 2, 3, 4, 5, 6,0];
-    let get: &Vec<i32> = &var1.iter().map(|f|{f*f}).collect();
-    print!("{:?}",get) ;
-
+    }
+    println!("{sss}")
 }
