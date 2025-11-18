8a00382d00422d922162269cd006c7b5d08e4d78
SAIRAJ KHILARI <sairajkhilari191918110+github1@gmail.com>
2025-11-18 13:08:38 +0530

    On branch working Your branch is up to date with 'origin/working'.
    Changes to be committed:
    	modified:   src/bin/arrays.rs

--- src/bin/arrays.rs
+++ src/bin/arrays.rs
@@ -1,17 +1,30 @@
 
 // * ARRAYS:
-            // ? arrays is a collection of same datatype of data
+            // ? arrays is a fixed-sized collection of homogenous data (same type of data)........
             // ? like int or floats or string 
             // ? arrays are not expandable
-            
+
 
 fn main(){
     
     // let a = ["hello","hi","how","are","you",5,5.5]; 
     // println!("{:?}",a) // ^ compilation error : expected `&str`, found integer
 
-    // let a = ["hello","hi","how","are","you"]; //^ ["hello", "hi", "how", "are", "you"]
-    // println!("{:?}",a) //^ ["hello", "hi", "how", "are", "you"]
+    let a = ["hello","hi","how","are","you"]; //^ ["hello", "hi", "how", "are", "you"]
+
+    println!("{:?}",a); //^ ["hello", "hi", "how", "are", "you"]
+
+    // * length  : check the length of the array
+    println!("{:?}",a.len()); //^ 5
+
+    // *
+    let b = a.copy_within(0,3);
+    println!("{:?}",b);
+
+
+
+
+
 
 
 }
