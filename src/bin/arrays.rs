1d7f6a69daffbd5dffc49e111db9e4a89acf819d
SAIRAJ KHILARI <sairajkhilari191918110+github1@gmail.com>
2025-11-21 07:57:09 +0530

    On branch working Your branch is up to date with 'origin/working'.
    Changes to be committed:
    	modified:   src/bin/arrays.rs

--- src/bin/arrays.rs
+++ src/bin/arrays.rs
@@ -1,8 +1,13 @@
 
+
+// * COMPOUND TYPE :
+                    // ? COLLECTION OF MULTIPLE VALUES
+
 // * ARRAYS:
             // ? arrays is a fixed-sized collection of homogenous data (same type of data)........
             // ? like int or floats or string 
-            // ? arrays are not expandable
+            // ? arrays are not expandable it is a fixeed size
+            // ? 
 
 
 fn main(){
@@ -10,22 +15,26 @@ fn main(){
     // let a = ["hello","hi","how","are","you",5,5.5]; 
     // println!("{:?}",a) // ^ compilation error : expected `&str`, found integer
 
-    let a = ["hello","hi","how","are","you"]; //^ ["hello", "hi", "how", "are", "you"]
+    let mut aa  = ["hello","hi","how","are","you"]; //^ ["hello", "hi", "how", "are", "you"]
 
-    println!("{:?}",a); //^ ["hello", "hi", "how", "are", "you"]
+    println!("{:?}",aa); //^ ["hello", "hi", "how", "are", "you"];
 
-    // * length  : check the length of the array
-    println!("{:?}",a.len()); //^ 5
+    // ? length  : check the length of the array
+    println!("{:?}",aa.len()); //^ 5
 
-    // *
+    // ?
     
     // let b = a.copy_within(0,3);
     // println!("{:?}",b);
 
 
+    // !  Cant change the size of array BUT!!!!!!!
+                // ? can change the items in the array
+                
 
-
-
+    println!("{:?}",aa);
+    aa[0] = "Hello!....";
+    println!("{:?}",aa);
 
 
 }
