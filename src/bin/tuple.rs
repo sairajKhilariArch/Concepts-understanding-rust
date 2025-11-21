9e152c43e0e9db56395c115c4eb1dfb87e12fc41
SAIRAJ KHILARI <sairajkhilari191918110+github1@gmail.com>
2025-11-21 21:15:49 +0530

    TUPLE On branch working Your branch is up to date with 'origin/working'.
    Changes to be committed:
    	modified:   src/bin/tuple.rs

---
+++ src/bin/tuple.rs
@@ -0,0 +1,39 @@
+// * TUPLE :
+            // ? tuple is a fixed, immutable, mixed types
+            
+            // ! to acess the data use "."
+
+            // ?pythons tuple is similar to the rusts tuple 
+
+            // ? index starts with 0
+
+            // ~ pythons tuple == rusts tuple
+
+            // ^ | Python | Rust  | Mutable? | Same Types? |
+            // ^ | ------ | ----- | -------- | ----------- |
+            // ^ | list   | Vec   | ✅ Yes   | Usually     |
+            // ^ | tuple  | tuple | ❎ No    | No          |
+
+            
+fn main(){
+
+    let t_uple = ("hi","hello","how","are","you",(2,4,6,8,10));
+
+    // let a =t_uple.5.3; //^ 8
+
+    let (a,b,c,d,e,f) = t_uple;
+
+    println!("{}",a);    //^ hi
+    println!("{}",b);    //^ hello
+    println!("{}",c);    //^ how
+    println!("{}",d);    //^ are 
+    println!("{}",e);    // ^ you
+    println!("{:#?}",f); // ^     (
+                         // ^     2,
+                         // ^     4,
+                         // ^     6,
+                         // ^     8,
+                         // ^     10,
+                         // ^ )
+    print!("");
+}
