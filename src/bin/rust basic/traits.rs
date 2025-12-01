8c3df6a5b71e4ed476462e153daa7e1a78709bef
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2025-12-01 18:27:06 +0530

    traits understanding On branch working Your branch is up to date with 'origin/working'.
    Changes to be committed:
    	modified:   src/bin/rust basic/traits.rs

--- src/bin/rust basic/traits.rs
+++ src/bin/rust basic/traits.rs
@@ -1,8 +1,8 @@
 
 // * TRAITS:
-            // ? Traits are like a CONTRACT if any one implement is 
-            // ?then they have to fill the given methodes 
-            // ?but if it is already written in trait then you dont have to specify  if you dont want to 
+            // ? Traits are like a CONTRACT if any one implement them 
+            // ?then they have to fill the given methodes with (require code) 
+            // ?but if it is already written((require code)) in trait then you do't have to specify  if you dont want to 
             
             // ! their are built in traits also like (debug , copy , clone ,etc...)
 
@@ -33,3 +33,19 @@ fn main() {
     println!("{}", emp.bonus_apply()); // Output: "Bonus: 5000"
     
 }
+
+
+
+// ? traits establish consistency between types;methods that represent the same behaviour have the dame name
+// ? when a type opts in to honoring a trait's requiements , we say the type implements the tratis.
+// ? types can vary in their implementation but still implement the trait.
+
+// ? types can vary in their implementation but still implement the same trait...
+
+// ? a type can choose to opting in to implementing a trait..
+
+// ? A trait can  implement multiple  traits there are hundreds of traits available in rust .
+
+// ? A trait is called  an interface or protocal  in other languages...
+
+// ? eg  a built in traits such as DISPLAY trait
