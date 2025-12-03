4224773760a67d93e13f99741a912f094deb4f39
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2025-12-03 08:47:44 +0530

    On branch working Your branch is up to date with 'origin/working'.
    Changes to be committed:
    	new file:   src/bin/control-flow-statement/control_flow.rs

---
+++ src/bin/control-flow-statement/control_flow.rs
@@ -0,0 +1,22 @@
+// * CONTROL FLOW STATEMENT :
+                            // ? control flow statement is refer to how a program will flow or execute or say the flow of a code written......
+
+                            // * if :
+
+                            // * else :
+
+                            // * else if :
+
+                            // * match statements :
+                                                
+                                                // ? The match statement can react to all possible variants of a value ....
+
+
+
+
+
+fn main(){
+    
+}
+
+                
