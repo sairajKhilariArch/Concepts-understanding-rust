4bd4d4fe75a7ee59e3713bf2745291f16807d475
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2025-12-22 08:01:04 +0530

    design pattern example On branch working Your branch is up to date with 'origin/working'.
    Changes to be committed:
    	modified:   src/bin/struct/design_pattern.rs
    	modified:   src/main.rs

--- src/bin/struct/design_pattern.rs
+++ src/bin/struct/design_pattern.rs
@@ -1,4 +1,73 @@
 // * Design pattern :
 //              ?  A Design pattern is a recommended way to write structure code to solve specific problem 
 
-//              ? 
+//              & in and out 
+//              & return type self 
+
+
+
+#[derive(Debug)] 
+
+struct Emp{
+    name : String,
+    age:u32,
+    designation: String,
+}
+
+
+impl Emp{
+    fn new(name:String,age:u32,designation:String)->Self{
+        Self{
+            name,
+            age,
+            designation,
+        }
+    }
+}
+
+impl Emp{
+    
+
+    fn details( self){
+        println!("{:#?}",self);
+    }
+    
+    fn change_designation(&mut self,cha:String)-> &mut self {
+        self.designation = cha;
+        println!("designation has change");
+        println!("{}",self.designation);
+        self
+    }
+    
+    fn change_age(&mut self,age:String)-> &mut Self {
+        self.age = age;
+        println!("age has change");
+        println!("{}",self.age);
+        self
+    }
+
+        fn change_name(&mut self,na:String)-> &mut Self {
+        self.name = na;
+        println!("name has change");
+        println!("{}",self.name);
+        self
+    }
+
+}
+
+
+
+fn main(){
+    let mut emp1 = Emp{
+        name : String::from("sairaj"),
+        age : 25,
+        designation : String::from("developer"),
+    };
+
+
+    emp1
+        .change_designation(String::from("rust developer"))
+        .change_age(26)
+        .change_name(String::from("Shadow"));
+
+} 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,72 @@
-fn main() {
+// fn main() {
 
-    println!("hello ")
+//     println!("hello ")
+// }
+
+
+#[derive(Debug)] 
+
+struct Emp{
+    name : String,
+    age:u32,
+    designation: String,
 }
 
+
+impl Emp{
+    fn new(name:String,age:u32,designation:String)->Self{
+        Self{
+            name,
+            age,
+            designation,
+        }
+    }
+}
+
+impl Emp{
+    
+
+    fn details( self){
+        println!("{:#?}",self);
+    }
+    
+    fn change_designation(&mut self,cha:String)-> &mut Self {
+        self.designation = cha;
+        println!("designation has change");
+        println!("{}",self.designation);
+        self
+    }
+    
+    fn change_age(&mut self,ag:u32)-> &mut Self {
+        self.age = ag;
+        println!("age has change");
+        println!("{}",self.age);
+        self
+    }
+
+        fn change_name(&mut self,na:String)-> &mut Self {
+        self.name = na;
+        println!("name has change");
+        println!("{}",self.name);
+        self
+    }
+
+}
+
+
+
+fn main(){
+    let mut emp1 = Emp{
+        name : String::from("sairaj"),
+        age : 25,
+        designation : String::from("developer"),
+    };
+
+
+    emp1
+        .change_designation(String::from("rust developer"))
+        .change_age(26)
+        .change_name(String::from("Shadow"));
+
+    emp1.details()
+} 
