71481a1ae363311d35edfe467373ccaa3a9d78d5
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2025-12-22 18:06:33 +0530

    On branch working Your branch is up to date with 'origin/working'.
    Changes to be committed:
    	modified:   src/bin/struct/tuple_field_struct.rs
    	new file:   src/bin/struct/unit_type_struct.rs

--- src/bin/struct/tuple_field_struct.rs
+++ src/bin/struct/tuple_field_struct.rs
@@ -1,7 +1,25 @@
-
+// this is a second type of struct 
 // * tuple field struct:
+//                  ? A tuple struct is a struct that assigns each piece  of data an order in line rather than a name....
 
 
 
 
 
+
+
+
+struct long_Duration(u32 , u32);
+
+struct short_Duration(u32,u32);
+
+fn main()  {
+
+    let my_shift = short_Duration(6,9);
+
+    let bros_shift = long_Duration(9,6);
+
+    println!("my shift start at {}pm and end at {}pm.",my_shift.0,my_shift.1 );  // ^ my shift start at 6pm and end at 9pm.
+
+    println!("bro's shift start at {}am and end at {}pm.",bros_shift.0,bros_shift.1); // ^ bro's shift start at 9am and end at 6pm.
+}
---
+++ src/bin/struct/unit_type_struct.rs
@@ -0,0 +1,12 @@
+// * Unite Type Struct :
+//              ?  A unit is an empty tuple ....,tuples without values...
+// 
+
+
+
+
+struct empty;
+
+fn main()  {
+    let my_empty_struct = empty;
+}
