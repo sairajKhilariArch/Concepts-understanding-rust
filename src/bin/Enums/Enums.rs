21a69f810c1eaed5868d110c64f01e184a098fa3
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2025-12-24 20:22:17 +0530

    On branch working Your branch is up to date with 'origin/working'.
    Changes to be committed:
    	modified:   .vscode/settings.json
    	modified:   src/bin/Enums/Enums.rs
    	modified:   src/bin/Enums/enum_match.rs

--- src/bin/Enums/Enums.rs
+++ src/bin/Enums/Enums.rs
@@ -3,7 +3,20 @@
 
 //              ? Enum means Enumerate
 
-//              ?
+//              ? Use the enum keyword followed by a name in PascalCase and Curly braces
+
+//              ? separate  variants with  a comma  and a line break...
+
+//              ? By default. custom enums do not implement the copy trait. ownership rules apply...
+
+
+
+// * Tuple Variant :
+//              ? A variant can store associated data..
+//              ? A Tuple Variant stores one or more pieces of data based on  order/position...
+
+//              ? In the variant definition, provide parentheses and the sequential type of the data....
+//              ? In the enum instance, provide parentheses and the sequential values for the declared types....
 
 
 
@@ -12,6 +25,17 @@
 //              ? A struct variant  Stores associated data fields rather than by position.  each piece of data has an associated name....
 //              ? struct as a variant for the enum type
 
+//              ? A STRUCT variant stores one or more pieces of data by field name ...
+//              ? in the variant definition , provide curly braces and a block ..inside the block ,declared the fields with their associated types ....
+
+//              ? for the enum instance ,provide curly braces and the concrete values for the fields .....
+
+
+// * Methods:
+//              ? Like Struct ,enums can define methods ..
+//              ? Use the impl keyword following by the enum name and a block /...
+//              ? Methods can take ownership of the enum or borrow it through a reference....
+//              ? Methods can define additional parameters after self..Pass those arguments in during invocation.....
 
 
 
@@ -83,7 +107,7 @@ fn main()  {
         Name: String::from("sairaj"),
         AccountNo:098765432123,
         Email:String::from("cus01@mail.com"),
-        Password:String::from("qwertyuiop"),
+        Password:String::from("1234567890"),
     };
 
     let customer01 = PaymentMethodType::Cred(customer01_cre);
--- src/bin/Enums/enum_match.rs
+++ src/bin/Enums/enum_match.rs
@@ -1,2 +1,71 @@
 
-// * match in enum 
+// * match in enum :
+//              ? The match keyword is often paired with enums ...
+//              ? The match keyword validates that eve enum variant has an associated code block....
+//              ? A match pattern can extract the associated data from a tuple variant or a struct variant .....
+//              ? A match pattern can match against an exact pieces of associated data....
+
+
+// * The if let Construct :
+//              ? The if let constant executes a block of code if there is a match against a specific enum variant ....
+//              ? The if let construct declares variables for the associated data .the block has access to the variables....
+//              ? Declare the hardcoded enum variant on the dynamic values on the right-hand side....
+
+
+
+
+
+#[derive(Debug)]
+enum Marvel{
+    Avengers,
+    Fantastic,
+    Defenders,
+    Shield,
+    Hydra,
+}
+
+impl Marvel{
+    // ? but  this is a marvel enum function only who has the enum variant  he can use this method
+    fn in_which_team(name:&Marvel) ->&str{
+        match name{
+            Marvel::Avengers => "you are in avengers team",
+            Marvel::Fantastic =>"you are in the Fantastic 4 team",
+            Marvel::Defenders => "you are in the Defenders",
+            Marvel::Shield => "you are in the Shield",
+            Marvel::Hydra => "you are in the Hydra",
+        }
+    }
+
+    fn in_avengers_team(self){
+        if let Marvel::Avengers = self {
+            println!("welcome  to avengers team")
+        }else {
+            println!("sorry,you are not in avengers team..you are in{:?}",self)
+        }
+    }
+
+}
+// ? this is a separate function and any one can use it
+fn in_which_team(name:&Marvel) ->&str{
+    match name{
+        Marvel::Avengers => "you are in avengers team",
+        Marvel::Fantastic =>"you are in the Fantastic 4 team",
+        Marvel::Defenders => "you are in the Defenders",
+        Marvel::Shield => "you are in the Shield",
+        Marvel::Hydra => "you are in the Hydra",
+    }
+}
+fn main()  {
+    // ? for a fn for match variant for variable
+    let ironman =Marvel::Avengers;
+    let mut team = in_which_team( &ironman);
+    println!("{:?}",team);
+
+    // ? implemented same function from impl method
+    let fury = Marvel::Shield;
+    team = &Marvel::in_which_team(&fury);
+    println!("{:?}",team);
+
+    fury.in_avengers_team();
+
+}
