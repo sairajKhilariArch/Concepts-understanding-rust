038d88e045a8910bfe9c0e55426166eadbd2b605
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2025-12-25 21:39:15 +0530

    On branch working Your branch is up to date with 'origin/working'.
    Changes to be committed:
    	modified:   .vscode/settings.json
    	modified:   src/bin/Generics/generics.rs
    	modified:   src/bin/struct/struct.rs

--- src/bin/Generics/generics.rs
+++ src/bin/Generics/generics.rs
@@ -1,3 +1,24 @@
 // * Generics:
 //              ? A Generic is a type argument ..is's a placeholder for future type...
 
+// * Turbofish Operator:
+//              ?   ::<i32>
+//              ? thi is hoe we show the built in operator....
+
+
+
+#[derive(Debug)]
+enum Cheesesteak<T>{
+    Plain,
+    Topping(T),
+}
+
+fn main()  {
+    let mushroom =Cheesesteak::Topping("mushroom");
+    let onions = Cheesesteak::Topping("onions");
+    println!("{:?}",mushroom);
+    println!("{:?}",onions);
+}
+
+
+
