50b0d04f6ca9867d172285cd352c280419befe89
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2025-12-26 17:15:07 +0530

    option_result_enum
    On branch working
    Your branch is ahead of 'origin/working' by 2 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   src/bin/Enums/option_result_enum.rs

---
+++ src/bin/Enums/option_result_enum.rs
@@ -0,0 +1,14 @@
+// * OPtion Enum:
+//              ? THe option enum models a scenario where a type could be a valid where could be a valid value or nothing at all .....
+
+
+// & Option::None :
+//              ? Represents an absent value...
+
+// & Option::Some(T):
+//              ? Represents a present value...
+
+// * Result Enum:
+//              ? The Result enum 
+
+
