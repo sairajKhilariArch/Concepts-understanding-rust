804ded1a722aebc72747c5a8f6c328e217e89ce5
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2025-12-28 18:54:18 +0530

    Result On branch working Your branch is ahead of 'origin/working' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/bin/memory-safety/Enums/Result.rs

--- src/bin/memory-safety/Enums/Result.rs
+++ src/bin/memory-safety/Enums/Result.rs
@@ -9,6 +9,12 @@
 // * Parse:
 //              ? it is same as the expect but it dose't stop the application from running it gives an error for if the wrong data type text is input in it
 //              ? same as the Err in the result enum*
+
+// ! Err also gives it in a wrap so after getting it we have ho unwrap it.. 
+// ! Ok variant gives the error as a present  and have to unwrap it..
+
+// ? so we can use the match statement to match if it is err or ok variant if we use the unwrap() method on it will unwrap it but also stop the application from running....
+
 fn main() {
     let hi: Result<i8,&str> = Result::Ok(5);
     println!("{hi:?}"); // ^ Ok(5)
@@ -23,5 +29,5 @@ fn main() {
     let text = "maharashtra";
     let text_as_number =text.parse::<i32>();
     println!("{:?}",text_as_number); // ^ Err(ParseIntError { kind: InvalidDigit })
-
+    
 }
