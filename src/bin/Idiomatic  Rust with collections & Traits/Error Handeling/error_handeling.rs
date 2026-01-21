75a6eafbbf248065b9508842b7707bad04de7820
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2026-01-21 22:03:25 +0530

    On branch working Your branch is up to date with 'origin/working'.
    Changes to be committed:
    	modified:   src/bin/Idiomatic  Rust with collections & Traits/Error Handeling/error_handeling.rs
    	modified:   src/main.rs

--- src/bin/Idiomatic  Rust with collections & Traits/Error Handeling/error_handeling.rs
+++ src/bin/Idiomatic  Rust with collections & Traits/Error Handeling/error_handeling.rs
@@ -1,9 +1,8 @@
-// * Error Handeling :
-//              ? Error Handeling is the procss of dealing with potential  errors from operations that can go wrong.....
+// * Error handling :
+//              ? Error handling is the process of dealing with potential  errors from operations that can go wrong.....
 
 // * Recoverable Error :
 //              ? Recoverable errors are errors that we can define code to handle..
-
 
 // * Unrecoverable Error:
 //              ? UNRecoverable Errors are errors that cause the program to be unable to process....
