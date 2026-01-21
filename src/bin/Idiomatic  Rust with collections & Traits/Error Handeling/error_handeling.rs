63e93a4d0cfc212ae32fcd6b3ddd5cdaab64e91c
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2026-01-21 22:01:59 +0530

    eprint! eprintln! error macro On branch working Your branch is up to date with 'origin/working'.
    Changes to be committed:
    	new file:   src/bin/Idiomatic  Rust with collections & Traits/Error Handeling/error_handeling.rs
    	new file:   src/bin/core-and-basic-concepts/macro/eprint.rs
    	modified:   src/main.rs

---
+++ src/bin/Idiomatic  Rust with collections & Traits/Error Handeling/error_handeling.rs
@@ -0,0 +1,12 @@
+// * Error Handeling :
+//              ? Error Handeling is the procss of dealing with potential  errors from operations that can go wrong.....
+
+// * Recoverable Error :
+//              ? Recoverable errors are errors that we can define code to handle..
+
+
+// * Unrecoverable Error:
+//              ? UNRecoverable Errors are errors that cause the program to be unable to process....
+
+// * BackTrace :
+//              ? Backtrace is the list of files and function that were running at the point that the error occurred..
---
+++ src/bin/core-and-basic-concepts/macro/eprint.rs
@@ -0,0 +1,31 @@
+// * eprint : standered error
+//              ? error print line Macro
+//              ? This macro prints the error line as output 
+//              ? like print macro their ar two of them ..:
+//              ^ 1. eprint!        2.eprintln!
+
+//              ~ cargo run > example.txt
+
+
+
+// & if i run this command 
+// ^ 󰣇 leraning/rust/Concepts-understanding-rust   working  !? ❯ cargo run > example.txt
+
+// ^ it will give out put to that example.txt file only print not the  error print
+
+// ^from this code 
+
+// ~    fn main() {
+// ~        println!("hello ");
+// ~        eprintln!("error");
+// ~    }
+
+//  ^ on therminal > error
+//  ^ in file > hello
+//  ^ this is the output and error it gives
+
+// ? success messages to the file 
+// ? error messages to the terminal
+
+
+
