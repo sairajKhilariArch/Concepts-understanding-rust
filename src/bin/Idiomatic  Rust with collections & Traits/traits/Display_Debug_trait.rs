c1facf23b74df96b3e91e3d8908772802c998359
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2026-02-06 20:49:10 +0530

    Debug Trait : for struct and enum  custamized On branch working Your branch is ahead of 'origin/working' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/bin/Idiomatic  Rust with collections & Traits/traits/Display_Debug_trait.rs

--- src/bin/Idiomatic  Rust with collections & Traits/traits/Display_Debug_trait.rs
+++ src/bin/Idiomatic  Rust with collections & Traits/traits/Display_Debug_trait.rs
@@ -7,6 +7,10 @@
 ?            to use a string print like a normal and code reusability we will implement the display trait directly to the struct with impl
 ?           as followed in the example........../
 
+* Debug Trait :
+?           It is same as the display but for the custimized version for the developer reading...
+
+
 
 */
 
@@ -110,3 +114,78 @@ fn main() {
 }
 
 */
+
+// * Debug trait for STRUCT and ENUM 
+
+/*
+
+use std::fmt::{Debug, Display, Formatter, Result};
+
+// ----------ENUM-----------------------------------------------------
+// ----------------------------------APPLE-KIND-------------------------
+// ---------------------------------------------------------------------
+
+enum AppleKind {
+    Japnese,
+    Kashmiri,
+    Canadien,
+}
+impl Display for AppleKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            AppleKind::Canadien => write!(f, "Canadien"),
+            AppleKind::Japnese => write!(f, "Japnese"),
+            AppleKind::Kashmiri => write!(f, "Kashmiri"),
+        }
+    }
+}
+
+impl Debug for AppleKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            AppleKind::Canadien => write!(f, "AppleKind::Canadien"),
+            AppleKind::Japnese => write!(f, "AppleKind::Japnese"),
+            AppleKind::Kashmiri => write!(f, "AppleKind::Kashmiri"),
+        }
+    }
+}
+
+// -----------STRUCT----------------------------------------------------
+// ----------------------------------APPLE------------------------------
+// ---------------------------------------------------------------------
+
+struct Apple {
+    kind: AppleKind,
+    price: i64,
+}
+
+impl Display for Apple {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{} ,{}", self.kind, self.price)
+    }
+}
+impl Debug for Apple {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "Apple ::: {:?} FOR {}", self.kind, self.price)
+    }
+}
+
+// ----------------------------------------------------------------------
+// ----------------------------------MAIN--------------------------------
+// ----------------------------------------------------------------------
+
+fn main() {
+    println!("hello ");
+
+    let a1 = Apple {
+        kind: AppleKind::Japnese,
+        price: 180,
+    };
+
+    println!("{a1:?}")
+}
+
+*/
+
+
+
