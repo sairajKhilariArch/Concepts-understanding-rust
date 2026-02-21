d41d0865c3d57ec819dc7b0105d888f5089273d1
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2026-02-21 08:13:06 +0530

    PartialOrd trait < > = on struct On branch working Your branch is up to date with 'origin/working'.
    Changes to be committed:
    	modified:   src/bin/Idiomatic  Rust with collections & Traits/traits/partialEq.rs

--- src/bin/Idiomatic  Rust with collections & Traits/traits/partialEq.rs
+++ src/bin/Idiomatic  Rust with collections & Traits/traits/partialEq.rs
@@ -2,6 +2,8 @@
 * PartialEq Trait:
 ?           The PartialEq trait establishes equality between two values.....
 
+* PartialOrd trait: ( Partial order trait)
+?           THe PartialOrd trait indicates that a type can be ordered/sorted.
 
 */
 
@@ -82,6 +84,74 @@ fn main() {
     println!("{}", b == c); // ^ true
     println!("{}", c == d); // ^ false
     println!("{}", d == a); // ^ false
+}
+
+*/
+
+//* PartialOrd on trait
+/*
+use std::cmp::Ordering;
+struct Job {
+    title: String,
+    salary: u128,
+}
+
+impl Job {
+    fn new(title: String, salary: u128) -> Self {
+        Self { title, salary }
+    }
+}
+
+impl PartialEq for Job {
+    fn eq(&self, other: &Self) -> bool {
+        self.salary == other.salary
+    }
+}
+
+impl Eq for Job {}
+
+impl PartialOrd for Job {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        //?
+        self.salary.partial_cmp(&other.salary)
+
+        // ? Match Approach
+        // match self.salary.partial_cmp(&other.salary){
+        //     Some(Ordering::Equal) => Some(Ordering::Equal),
+        //     Some(Ordering::Greater) => Some(Ordering::Greater),
+        //     Some(Ordering::Less) => Some(Ordering::Less),
+        //     None => None
+        // }
+
+        // ? if else approach
+        //     if self.salary == other.salary {
+        //         Some(Ordering::Equal)
+        //     } else if self.salary < other.salary {
+        //         Some(Ordering::Less)
+        //     } else if self.salary > other.salary {
+        //         Some(Ordering::Greater)
+        //     } else {
+        //         None
+        //     }
+    }
+}
+
+fn main() {
+    let first_job = Job::new(String::from("DEV"), 100000);
+    let second_job = Job::new(String::from("HR"), 80000);
+    let third_job = Job::new(String::from("DESIGNER"), 100000);
+
+    println!("{}", first_job == third_job); // ^ true
+    println!("{}", first_job < third_job); // ^ false
+    println!("{}", first_job > third_job); // ^ false
+    println!("{}", first_job <= third_job); // ^ true
+    println!("{}", first_job >= third_job); // ^ true
+
+    println!("{}", first_job == second_job); // ^ false
+    println!("{}", first_job > second_job); // ^ true
+    println!("{}", first_job < second_job); // ^ false
+    println!("{}", first_job >= second_job); // ^ true
+    println!("{}", first_job <= second_job); // ^ false
 }
 
 */
